e2501e51925222e044bf89967e3aa74e13dee257
igzune <herisotlla@outlook.com>
2020-06-18 22:12:57 -0500

    Archivo de prueba constructor agregado

---
+++ clase_constructor.py
@@ -0,0 +1,32 @@
+class Persona():
+    pass
+
+    def __init__(self, nombre, ano):
+        self.nombre = nombre
+        self.ano = ano
+        pass
+
+    def descripcion(self):
+        return '{} tiene {} anios'.format(self.nombre, self.ano)
+
+    def comentario(self, frase):
+        return '{} dijo {}'.format(self.nombre, frase)
+
+
+doctor = Persona('Jose', 26)
+print(doctor.descripcion())
+print(doctor.comentario('Hola mundo!!'))
+
+
+class email():
+    def __init__(self):
+        self.enviado = False
+
+    def enviarCorreo(self):
+        self.enviado = True
+
+
+miCorreo = email()
+print(miCorreo.enviado)
+miCorreo.enviarCorreo()
+print(miCorreo.enviado)
